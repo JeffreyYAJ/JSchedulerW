0c9e9532b942aaf68ed415899ee301d53fbc4fea
JeffreyYAJ <jeffreyyaj@gmail.com>
2026-03-14 14:10:30 +0100

    building app

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { fork } = require('child_process');
 
 let mainWindow;
+let backendProcess;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -12,11 +15,31 @@ function createWindow() {
     },
   });
 
-  // On charge le frontend Vite qui tourne déjà grâce à concurrently
-  mainWindow.loadURL('http://localhost:5173'); 
+  // Détection du mode (Dev ou Prod)
+  if (!app.isPackaged) {
+    // Mode DÉVELOPPEMENT
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    // Mode PRODUCTION
+    mainWindow.loadFile(path.join(__dirname, 'JSchedulerW_frontend', 'dist', 'index.html'));
+  }
 }
 
 app.whenReady().then(() => {
+  // Lancement du backend uniquement en production (en dev, concurrently s'en charge)
+  if (app.isPackaged) {
+    const backendPath = path.join(__dirname, 'JSchedulerW_backend', 'server.js'); // Vérifie que c'est bien server.js !
+    
+    backendProcess = fork(backendPath, [], {
+      cwd: path.join(__dirname, 'JSchedulerW_backend'),
+      env: { ...process.env, NODE_ENV: 'production' }
+    });
+
+    backendProcess.on('error', (err) => {
+      console.error('Erreur du backend en production:', err);
+    });
+  }
+
   createWindow();
 
   app.on('activate', function () {
@@ -28,59 +51,9 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
 
-// const { app, BrowserWindow } = require('electron');
-// const path = require('path');
-// const { fork } = require('child_process');
-// 
-// let mainWindow;
-// let serverProcess;
-// 
-// function createWindow() {
-//   mainWindow = new BrowserWindow({
-//     width: 1280,
-//     height: 800,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       contextIsolation: false,
-//     },
-//   });
-// 
-//   const isDev = !app.isPackaged;
-// 
-//   if (isDev) {
-//     mainWindow.loadURL('http://localhost:5173'); 
-//   } else {
-//     mainWindow.loadFile(path.join(__dirname, 'JSchedulerW_frontend', 'dist', 'index.html')); 
-//   }
-// }
-// 
-// app.whenReady().then(() => {
-//   // 1. DÉMARRER LE BACKEND (On pointe vers le sous-dossier 'backend')
-//   const backendPath = path.join(__dirname, 'JSchedulerW_backend', 'server.js'); s
-//   
-//   // On lance le backend avec fork
-//   serverProcess = fork(backendPath, [], {
-//     cwd: path.join(__dirname, 'JSchedulerW'), // Très important: définit le dossier de travail du backend
-//     env: process.env 
-//   });
-// 
-//   serverProcess.on('error', (err) => {
-//     console.error('Erreur de démarrage du backend:', err);
-//   });
-// 
-//   // 2. CRÉER LA FENÊTRE
-//   createWindow();
-// 
-//   app.on('activate', function () {
-//     if (BrowserWindow.getAllWindows().length === 0) createWindow();
-//   });
-// });
-// 
-// app.on('window-all-closed', function () {
-//   if (process.platform !== 'darwin') app.quit();
-// });
-// app.on('before-quit', () => {
-//   if (serverProcess) {
-//     serverProcess.kill();
-//   }
-// });
+// Arrêter le backend quand l'utilisateur ferme l'appli
+app.on('before-quit', () => {
+  if (backendProcess) {
+    backendProcess.kill();
+  }
+});
